5f21e167176d5bbc7669109e11ed33a3144df457
夏一飞 <variantconst@VCsMBA.local>
2024-11-24 21:34:22 +0800

    chore: update environment variables and scripts

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# 等待 PostgreSQL 服务启动
+# 如果使用外部数据库，等待外部 PostgreSQL 服务启动
 echo "Waiting for PostgreSQL to start..."
-while ! nc -z db 5432; do
+while ! nc -z ${POSTGRES_HOST:-db} ${POSTGRES_PORT:-5432}; do
   sleep 1
 done
 echo "PostgreSQL is up!"
 
 # 创建数据库（如果不存在）
 echo "Creating database if not exists..."
-PGPASSWORD=$POSTGRES_PASSWORD psql -h db -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'openwebui_monitor'" | grep -q 1 || \
-PGPASSWORD=$POSTGRES_PASSWORD psql -h db -U postgres -c "CREATE DATABASE openwebui_monitor"
+PGPASSWORD=${POSTGRES_PASSWORD} psql -h ${POSTGRES_HOST:-db} -p ${POSTGRES_PORT:-5432} -U ${POSTGRES_USER:-postgres} -tc "SELECT 1 FROM pg_database WHERE datname = '${POSTGRES_DATABASE:-openwebui_monitor}'" | grep -q 1 || \
+PGPASSWORD=${POSTGRES_PASSWORD} psql -h ${POSTGRES_HOST:-db} -p ${POSTGRES_PORT:-5432} -U ${POSTGRES_USER:-postgres} -c "CREATE DATABASE ${POSTGRES_DATABASE:-openwebui_monitor}"
 echo "Database setup completed!"
 
 # 等待新创建的数据库准备就绪
